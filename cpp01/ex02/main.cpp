d1dc86973db79871e4a9c0bd8e95c83537888ac2
jgrimaud <jgrimaud@student.42lyon.fr>
2024-08-26 03:20:17 +0200

    ex02

---
+++ cpp01/ex02/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+int main()
+{
+    std::string str = "HI THIS IS BRAIN";
+    std::string &stringREF = str;
+    std::string *stringPTR = &str;
+
+    std::cout << "&str       : " << &str << std::endl;
+    std::cout << "&stringREF : " << &stringREF << std::endl;
+    std::cout << "stringPTR  : " << stringPTR << std::endl;
+
+    std::cout << "str        : " << str << std::endl;
+    std::cout << "stringREF  : " << stringREF << std::endl;
+    std::cout << "*stringPTR : " << *stringPTR << std::endl;
+
+    return 0;
+}
