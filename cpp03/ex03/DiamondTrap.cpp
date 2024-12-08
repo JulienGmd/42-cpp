e2f3f6e45b25ffe7c4e414caba9368007a05ac64
jgrimaud <jgrimaud@student.42lyon.fr>
2024-12-08 18:47:10 +0100

    remove -O3 from makefiles and "temporary" fix for diamond trap

--- cpp03/ex03/DiamondTrap.cpp
+++ cpp03/ex03/DiamondTrap.cpp
@@ -3,11 +3,14 @@
 
 DiamondTrap::DiamondTrap() : ClapTrap("default_clap_name"), FragTrap(), ScavTrap()
 {
-    // TODO Marche pas car les 3 attackDamage ont la meme valeur (celle de ScavTrap)
     name = "default";
-    health = FragTrap::health;
-    energy = ScavTrap::energy;
-    attackDamage = FragTrap::attackDamage;
+    // Note: baseClass::var doesn't work for some reason.
+    // health = FragTrap::health;
+    // energy = ScavTrap::energy;
+    // attackDamage = FragTrap::attackDamage;
+    health = 100;
+    energy = 50;
+    attackDamage = 30;
     std::cout << "DiamondTrap " << name << " created";
     ScavTrap::printStats();
 }
@@ -15,15 +18,14 @@ DiamondTrap::DiamondTrap() : ClapTrap("default_clap_name"), FragTrap(), ScavTrap
 DiamondTrap::DiamondTrap(const std::string &name)
     : ClapTrap(name + "_clap_name"), FragTrap(name), ScavTrap(name)
 {
-    // TODO Marche pas car les 3 attackDamage ont la meme valeur (celle de ScavTrap)
-    std::cout << "Claptrap attackDamage: " << ClapTrap::attackDamage << std::endl;
-    std::cout << "Fragtrap attackDamage: " << FragTrap::attackDamage << std::endl;
-    std::cout << "Scavtrap attackDamage: " << ScavTrap::attackDamage << std::endl;
-
     this->name = name;
-    health = FragTrap::health;
-    energy = ScavTrap::energy;
-    attackDamage = FragTrap::attackDamage;
+    // Note: baseClass::var doesn't work for some reason.
+    // health = FragTrap::health;
+    // energy = ScavTrap::energy;
+    // attackDamage = FragTrap::attackDamage;
+    health = 100;
+    energy = 50;
+    attackDamage = 30;
     std::cout << "DiamondTrap " << name << " created";
     ScavTrap::printStats();
 }
